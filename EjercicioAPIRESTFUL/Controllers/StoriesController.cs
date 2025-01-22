e9c97817538c32b79c6c07f8d3760dff8d69b135
demian35 <josedemian@ciencias.unam.mx>
2025-01-21 22:04:04 -0600

    Agregando sentencia NameSpace

--- EjercicioAPIRESTFUL/Controllers/StoriesController.cs
+++ EjercicioAPIRESTFUL/Controllers/StoriesController.cs
@@ -2,25 +2,32 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-[Route("api/[controller]")]
-[ApiController]
-public class StoriesController : ControllerBase{
-    private readonly HackerNewsService _hackerNewsService;
+namespace EjercicioAPIRESTFUL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoriesController : ControllerBase
+    {
+        private readonly HackerNewsService _hackerNewsService;
 
-    //constructor del controlador
-    public StoriesController(HackerNewsService hackerNewsService){
-        _hackerNewsService= hackerNewsService;
-    }
-
-    //Metodo que recibe peticiones get para obtener las historias
-    [HttpGet]
-    public async Task<IActionResult> GetTopStories([FromQuery] int n=10){
-        if(n<=0){
-            return BadRequest("The number of stories must be better than 0");
+        //constructor del controlador
+        public StoriesController(HackerNewsService hackerNewsService)
+        {
+            _hackerNewsService = hackerNewsService;
         }
-        var stories= await _hackerNewsService.GetTopStoriesAsync(n);
-        return Ok(stories);
+
+        //Metodo que recibe peticiones get para obtener las historias
+        [HttpGet]
+        public async Task<IActionResult> GetTopStories([FromQuery] int n = 10)
+        {
+            if (n <= 0)
+            {
+                return BadRequest("The number of stories must be better than 0");
+            }
+            var stories = await _hackerNewsService.GetTopStoriesAsync(n);
+            return Ok(stories);
+        }
+
+
     }
-
-
 }
