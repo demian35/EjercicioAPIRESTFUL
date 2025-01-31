29e9cf9ebf567b6042fa9c76b9960a775c896843
demian35 <josedemian@ciencias.unam.mx>
2025-01-30 19:15:59 -0600

    documentando en ingles la clase Story.cs

--- EjercicioAPIRESTFUL/Models/Story.cs
+++ EjercicioAPIRESTFUL/Models/Story.cs
@@ -1,9 +1,10 @@
 //clase que modelara las historias que manejara el API desde La API de Hacker News
+//model class that model a note of the Hacker News Api in the api
 public class Story{
-    public string Title{get; set;} //titulo de la historia
-    public string URI{get; set;}//URI de la Historia
-    public string PostedBy{get; set;}// Autor 
-    public DateTime Time{get; set;}//fecha de publicacion
-    public int Score{get; set;}//puntuacion de la historia
-    public int CommentCount{get; set;}//cantidad de comentarios
+    public string Title{get; set;} //titulo de la historia. Title of the Story
+    public string URI{get; set;}//URI de la Historia. Uri of the Story
+    public string PostedBy{get; set;}// Autor. Author
+    public DateTime Time{get; set;}//fecha de publicacion. Date of the posted
+    public int Score{get; set;}//puntuacion de la historia. Score of Story
+    public int CommentCount{get; set;}//cantidad de comentarios. Number of comments
 }
